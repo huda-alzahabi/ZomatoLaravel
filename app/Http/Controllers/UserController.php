eadb8ef93d55da43892b404fc5ed53e4c1f2c667
huda-alzahabi <hudazahabi00@gmail.com>
2022-06-07 19:42:50 +0300

    Implemented signup functionality

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+
+    public function signUp(Request $request, $user_type_id){
+        $user = [];
+        $user["name"] = $request->name;
+        $user["email"] = $request->email;
+        $user["password"] = $request->password;
+        $user["user_type_id"] = $user_type_id;
+
+        return response()->json([
+            "status" => "Success",
+            "users" => $user
+        ], 200);
+    }
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\RestoController;
 
-
+Route::post('/register/{user_type_id}', [UserController::class, 'signUp']);
 Route::post('/add_resto', [RestoController::class, 'addResto']);
-
