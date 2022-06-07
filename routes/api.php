8ebc80b1536780b7b60fbf55b1bb4c8d5dd85aaa
huda-alzahabi <hudazahabi00@gmail.com>
2022-06-07 19:31:29 +0300

    Implemented addResto function

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\RestoController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+Route::post('/add_resto', [RestoController::class, 'addResto']);
+
