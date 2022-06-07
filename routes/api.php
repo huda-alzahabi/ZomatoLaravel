d4b75e389390d79c1656d0ed17560967e6c5cc79
huda-alzahabi <hudazahabi00@gmail.com>
2022-06-07 19:54:02 +0300

    Added the controllers to separate folders

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\AdminController;
+use App\Http\Controllers\User\UserController;
+use App\Http\Controllers\Admin\AdminController;
 
 Route::post('/register/{user_type_id}', [UserController::class, 'signUp']);
 Route::post('/add_resto', [AdminController::class, 'addResto']);
