a84230ecf398ae76d08d4a785ae0b833e3a711aa
huda-alzahabi <hudazahabi00@gmail.com>
2022-06-07 18:51:58 +0300

    Created and migrated the database tables

--- database/migrations/2014_10_12_000000_create_reviews_table.php
+++ database/migrations/2014_10_12_000000_create_reviews_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateReviewsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('reviews', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->integer('usertype_id');
+            $table->string('stars');
+            $table->string('comments');
+            $table->integer('user_id');
             $table->integer('restaurant_id');
+            $table->integer('status');
             $table->rememberToken();
             $table->timestamps();
         });
--- database/migrations/2014_10_12_000000_create_usertypes_table.php
+++ database/migrations/2014_10_12_000000_create_usertypes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateUserTypesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('usertypes', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->integer('usertype_id');
-            $table->integer('restaurant_id');
             $table->rememberToken();
             $table->timestamps();
         });
