82f559f6e76522587d6f36a44e921302494bc4c2
huda-alzahabi <hudazahabi00@gmail.com>
2022-06-07 19:30:49 +0300

    Implemented addResto function

--- app/Http/Controllers/RestoController.php
+++ app/Http/Controllers/RestoController.php
@@ -4,7 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class RestoController extends Controller
-{
-    //
+use App\Models\Restaurant;
+
+class RestoController extends Controller{
+
+
+    public function addResto(Request $request){
+        $resto = new Restaurant;
+        $resto->name = $request->name;
+        $resto->description = $request->description;
+        $resto->save();
+
+        return response()->json([
+            "status" => "Success"
+        ], 200);
+    }
+
 }
